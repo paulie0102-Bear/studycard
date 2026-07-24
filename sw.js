8584959580aa4bdac8b4126f4e375b521fe09968
paulie0102-Bear <paulie0102@gmail.com>
2026-07-25 00:18:11 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'smartcards-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  'https://raw.githubusercontent.com/paulie0102-Bear/studycard/refs/heads/main/paw.png'
+];
+
+// 安裝並快取檔案
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
+  );
+});
+
+// 攔截網路請求，優先使用快取內容
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
